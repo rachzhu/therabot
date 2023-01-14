5ffc87cc2385eb58371e07a993d2f46c13f23413
abalteanu <anabalteanu2003@gmail.com>
2023-01-14 15:53:46 -0500

    Prediction and print with streamlit (web app)

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+import cohere
+import streamlit as st
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+st.write("HELLO WORLD")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+co = cohere.Client('knET4NqdcT9EKSuN0tkxYpdrxHOu2MutKZ7n7IRk')
+response = co.generate(
+    model='xlarge',
+    prompt='This program generates mental health coping mechanisms from simple commands given an problem. Here are some examples:\nCommand: I am depressed\nEmotion: depression\nResponse: When feeling depressed, go eat something sweet\n--\nCommand: I am suicidal\nEmotion: suicide\nResponse: Let\'s start off by listing the things you are greatful for\n--\nCommand: I am having an anxiety attack\nEmotion: anxiety\nResponse: Start by taking some deep breaths. \n--\nCommand: I am panicking\nEmotion: panic\nResponse: Imagine you are somewhere calm, like the beach.\n--\nCommand: I am nervous\nEmotion: nervous\nResponse: Hold your hands under cold water\n--\nCommand: I need strategies to help with my depression',
+    max_tokens=50,
+    temperature=0.9,
+    k=0,
+    p=0.75,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop_sequences=[],
+    return_likelihoods='NONE')
+print('Prediction: {}'.format(response.generations[0].text))
+st.write('Prediction: {}'.format(response.generations[0].text))
